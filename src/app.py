8fb1ef77a0ef6d97efdf35f24190ecee86a6cefa
Pedro Hemrique Lima Silva <pedrolima828@gmail.com>
2025-06-02 19:40:02 -0300

    @REIS-V0.1: config cors in project

--- src/app.py
+++ src/app.py
@@ -1,15 +1,16 @@
-from typing import Union
-
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
